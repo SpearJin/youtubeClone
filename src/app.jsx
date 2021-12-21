8c13f158b8726705819783d55c40f4c4e397d4c2
SpearJin <ckdwls2525@naver.com>
2021-12-21 23:52:21 +0900

    show video list with title

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from 'react';
 import './app.css';
+import VideoList from './components/video_list/video_list';
 
 function App() {
-  return <div>헬로~~</div>;
+  const [video, setVideo] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+
+    fetch(
+      'https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyCprNTAndCSXCCRsl_gGXwa6cYIcwO2GVA',
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideo(result.items))
+      .catch((error) => console.log('error', error));
+  }, []);
+  return <VideoList videos={video} />;
 }
 
 export default App;
